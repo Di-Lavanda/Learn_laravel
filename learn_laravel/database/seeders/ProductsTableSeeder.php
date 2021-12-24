e56af47ff587ca8e4c232a44bd2525688d1277a8
Di-Lavanda <50248066+Di-Lavanda@users.noreply.github.com>
2021-12-24 04:50:14 +0400

    initial commit

---
+++ learn_laravel/database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i < 11; $i++)
+        DB::table('products')->insert([
+            'title' => 'Product '.$i,
+            'price' => rand(100, 1500),
+            'in_stock' => 1,
+            'description' => 'some description',
+        ]);
+    }
+}
---
+++ learn_laravel/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::get('/category/{cat}','App\Http\Controllers\ProductController@showCategory')->name('showCategory');
+Route::get('/category/{cat}/{product_id}','App\Http\Controllers\ProductController@show')->name('showProduct');
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name('cartIndex');
+Route::post('/add-to-cart', 'App\Http\Controllers\CartController@addToCart')->name('addToCart');
